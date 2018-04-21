b02d14b546d75a3b384cf2ebac8040e7c622d036
lukeo <lukeoleson@gmail.com>
2018-04-21 09:29:50 -0700

    started tracking the config folder

---
+++ app/config/config.php
@@ -0,0 +1,14 @@
+<?php
+
+    // db paramas
+    define('DB_HOST', 'localhost');
+    define('DB_USER', '_YOUR_USER_');
+    define('DB_PASS', '_YOUR_PASS_');
+    define('DB_NAME', '_YOUR_DB_NAME');
+
+    // app root
+    define('APPROOT', dirname(dirname(__FILE__)));
+    // URL root
+    define('URLROOT', '_YOUR_URL_');
+    // site name
+    define('SITENAME', '_YOUR_SITENAME_');
